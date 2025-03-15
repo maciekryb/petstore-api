ab18cba792b44bb55dd91e65a2a5838094708a70
Maciej Rybak <maciek.rybak95@gmail.com>
2025-03-15 19:23:05 +0100

    Add pet management routes to the application

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\PetController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/pets');
 });
+
+//Main page
+Route::get('/pets', [PetController::class, 'index']);
+//Form to add new pet
+Route::get('/pets/create', [PetController::class, 'create']);
+//Form to save new pet
+Route::post('/pets', [PetController::class, 'store']);
+//Form to get pet by id
+Route::get('/pets/{id}', [PetController::class, 'show']);
+//Form to edit pet by id
+Route::get('/pets/{id}/edit', [PetController::class, 'edit']);
+//Form to update pet by id
+Route::put('/pets/{id}', [PetController::class, 'update']);
+//Form to delete pet by id
+Route::delete('/pets/{id}', [PetController::class, 'destroy']);
