7169eac4f4e79ace0c8754d3a66ac18ef799476d
Maciej Rybak <maciek.rybak95@gmail.com>
2025-03-15 22:57:30 +0100

    Add form for retrieving pet details by identification number in index view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,4 +5,11 @@
 
     <a href="{{ route('pets.create') }}" class="btn btn-primary">Dodaj zwierzaka!</a>
 
+    <form action="" method="GET" class="mt-3"
+        onsubmit="event.preventDefault(); window.location.href = '{{ url('/pets') }}/' + document.getElementById('identificationNumber').value;">
+        <label for="identificationNumber">Sprawdź dane zwierzaka:</label>
+        <input type="number" id="identificationNumber" name="identificationNumber" class="form-control"
+            placeholder="Wpisz ID " required>
+        <button type="submit" class="btn btn-secondary mt-2">Pokaż szczegóły</button>
+    </form>
 @endsection
