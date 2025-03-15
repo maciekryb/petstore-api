0fc993b4266cde4c8ac60525142cf56ca9e45e22
Maciej Rybak <maciek.rybak95@gmail.com>
2025-03-15 22:06:27 +0100

    Add PetService class for handling pet data storage

---
+++ app/Services/PetService.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace App\Services;
+
+use App\Clients\PetApiClient;
+
+class PetService
+{
+    public function __construct(private PetApiClient $petApiClient) {}
+
+    public function store($data)
+    {
+
+        $preparedData =  [
+            // 'id' => $data['id'],
+            'name' => $data['name'],
+            // 'category' => [
+            //     'id' => $data['category_id'],
+            //     'name' => $this->getCategoryName($data['category_id']),
+            // ],
+            'photoUrls' => $data['photoUrls'],
+            // 'tags' => $tags,  // Zmieniliśmy na tablicę z tagiem
+            // 'status' => $data['status'],
+        ];
+
+        $response = $this->petApiClient->store($data);
+        return $response;
+    }
+}
