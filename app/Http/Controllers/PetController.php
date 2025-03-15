591f93ecf4845082d17ae51b55fe1b8764486102
Maciej Rybak <maciek.rybak95@gmail.com>
2025-03-15 22:06:15 +0100

    Add store method to PetController for handling pet creation with validation

--- app/Http/Controllers/PetController.php
+++ app/Http/Controllers/PetController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Services\PetDataService;
+use App\Services\PetService;
+// use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 
 class PetController extends Controller
 {
-    public function index(){
+
+    public function __construct(private PetService $petService) {}
+
+    public function index()
+    {
         return view('index');
     }
 
@@ -18,4 +25,31 @@ class PetController extends Controller
         return view('pets/create', compact('categories', 'tags'));
     }
 
+    public function store(Request $request)
+    {
+        logger($request->all());
+        // Walidacja danych z formularza
+        $validated = $request->validate([
+            'name' => 'required|string',
+            'photoUrls' => 'required|string',
+            'identificationNumber' => 'sometimes|integer|nullable',
+            'category_id' => 'sometimes|integer',
+            'tag_id' => 'sometimes|integer',
+            'status' => 'sometimes|string',
+        ], [
+            'name.required' => 'Imię jest wymagane.',
+            'photoUrls.required' => 'Adresy zdjęć są wymagane.',
+            'identificationNumber.integer' => 'Numer identyfikacyjny musi być liczbą całkowitą.',
+            'category_id.integer' => 'Wskazana kategoria nie istnieje.',
+            'tag_id.integer' => 'Wskazana tag nie istnieje.',
+        ]);
+
+        $response = $this->petService->store($validated);
+        logger($response);
+        if ($response) {
+            return redirect()->route('pet.index')->with('success', 'Pet added successfully');
+        } else {
+            return redirect()->back()->withInput()->with('error', 'Wystąpił błąd w zewnętrznej usłudze, prosze spróbować później');
+        }
+    }
 }
