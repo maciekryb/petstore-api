4ee71efdba3e94766ddc5f0a754d10e726468788
Maciej Rybak <maciek.rybak95@gmail.com>
2025-03-16 11:29:26 +0100

    Replace inline error display with validation error component in pet show view

--- resources/views/pets/show.blade.php
+++ resources/views/pets/show.blade.php
@@ -29,14 +29,8 @@
             </form>
         </div>
 
-        @if ($errors->any())
-            <div style="border: 1px solid red; padding: 10px; margin-top: 20px; color: red !important;">
-                <ul style="list-style-type: none; padding: 0; margin: 0;">
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
+
+
+        <x-validation-errors />
     @endif
 @endsection
